ab8ef7f60decb0144c2af616d363ed11080cf405
João Barbosa <bmpj13@hotmail.com>
2016-11-29 23:40:16 +0000

    working on home's CSS

--- templates/header.php
+++ templates/header.php
@@ -2,18 +2,18 @@
   <head>
     <title>Restaurant Guide</title>
     <meta charset="utf-8">
-   <!-- <link rel="stylesheet" href="css/style.css"> -->
+   	<link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 	  <?php
 	  	session_start();
 	  ?>
-	  
+
 	  <header>
 		  <a href="home.php" id="home">Restaurant Guide</a>
 		  <input type="text" id="textSearch" placeholder="Search RestaurantGuide..."/>
 		  <input type="button" id="btnSearch" value="Search" />
-		  
+
 		  <?php
 			if (isset($_SESSION['username'])) {
 				echo "hehe";
@@ -21,35 +21,6 @@
 			else {
 				echo "<a>Login</a>";
 				echo "<a>Register</a>";
-			} 
+			}
 		  ?>
 	  </header>
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
--- templates/home.php
+++ templates/home.php
@@ -1,19 +1,23 @@
-<section id="top_rated">
-	<?php foreach($topRestaurants as $restaurant) { ?>
-		<article class="restaurant">
-			<h3><?=$restaurant['name']?></h3>
-			<p><?=$restaurant['location']?></p>
-			<p><?=$restaurant['category']?></p>
-		</article>
-	<?php } ?>
-</section>
+<div class="container">
+	<section id="top_rated">
+		<h1>Top reviews</h1>
+		<?php foreach($topRestaurants as $restaurant) { ?>
+			<article class="restaurant">
+				<h3><?=$restaurant['name']?></h3>
+				<p><?=$restaurant['location']?></p>
+				<p><?=$restaurant['category']?></p>
+			</article>
+		<?php } ?>
+	</section>
 
-<section id="most_recent">
-	<?php foreach($recentRestaurants as $restaurant) { ?>
-		<article class="restaurant">
-			<h3><?=$restaurant['name']?></h3>
-			<p><?=$restaurant['location']?></p>
-			<p><?=$restaurant['category']?></p>
-		</article>
-	<?php } ?>
-</section>
+	<section id="most_recent">
+		<h1>Most recent</h1>
+		<?php foreach($recentRestaurants as $restaurant) { ?>
+			<article class="restaurant">
+				<h3><?=$restaurant['name']?></h3>
+				<p><?=$restaurant['location']?></p>
+				<p><?=$restaurant['category']?></p>
+			</article>
+		<?php } ?>
+	</section>
+</div>
