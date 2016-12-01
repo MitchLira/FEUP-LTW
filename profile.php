317b5d81c28941dd64c0fb3e42b3f2f283bd762b
Miguel Lira Barbeitos Luís <migueliraluis@hotmail.com>
2016-12-01 16:55:47 +0000

    abc

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+	include_once('database/connection.php');
+	include_once('database/user.php');
+	include_once('database/restaurants.php');
+	include_once('database/reviews.php');
+	
+	
+	$username = $_GET['username'];
+	try{
+		$userProfile = getUser($dbh, $username);
+	} catch(PDOException $e) {
+		die($e->getMessage());
+	}
+	
+	include('templates/header.php');
+	include('templates/profile.php');
+	include('templates/footer.php');
+?>
---
+++ templates/profile.php
@@ -0,0 +1,54 @@
+<section id="namesInfo">
+	<h3 id="name"><?=$userProfile['name']?></h3>
+	<h4 id='username'>@<?=$userProfile['username']?></h4>
+	
+	<?php if($userProfile['status'] == 'owner'){ ?>
+			<p id="status">Owner</p>
+	<?php	} else if($userProfile['status'] == 'reviewer'){ ?>
+			<p id="status">Reviewer</p>
+	<?php	} else{ ?>
+			<p id="status">User</p>
+	<?php	} ?>
+</section>
+
+<section id="personalInfo">
+	<h4> Personal Information </h4>
+	<p id="email">Email: <?=$userProfile['email']?></p>
+	<p id="country">Country: <?=$userProfile['country']?></p>
+	<p id="birthday">Birthday: <?=$userProfile['birthday']?></p>
+</section>
+
+<?php
+	if($userProfile['status'] == 'owner'){
+		$ownerRestaurants = getOwnerRestaurants($dbh, $userProfile['username']);
+	?>
+	
+	<section class="restaurants">
+		<h4><?=$userProfile['name'] ?> restaurants</h4>
+	
+		<?php foreach($ownerRestaurants as $rest){ ?>
+				<p> &#10039 <?=$rest['name']?></p>
+		<?php } ?>
+	</section>
+	
+<?php
+	}
+	else if($userProfile['status'] == 'reviewer'){
+		$reviewerRestaurants = getReviewerRestaurants($dbh, $userProfile['username']);
+	?>
+	
+	<section class="reviews">
+		<h4><?=$userProfile['name']?> reviews</h4>
+	
+		<?php foreach($reviewerRestaurants as $rest){ 
+				$nameRest = getRestaurantById($dbh, $rest['idRestaurant']); ?>
+				
+				<h5> &#10039 <?=$nameRest['name']?></h5>
+				<p>  &#10137 <?=$rest['fulltext']?></p>
+				<p>  &#10137 Rating: <?=$rest['rating']?></p>
+		<?php } ?>
+	</section>
+
+<?php	
+	}
+?>
