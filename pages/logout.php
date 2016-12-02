87b7e1781197cd499e469ac15f58809d8d6d94e9
Miriam Gonçalves <margaitizinha_cbt@hotmail.com>
2016-12-02 22:41:50 +0000

    header finally completed

--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,5 @@
 <?php
 	$linkAddress = "../pages/profile.php?username=" . $_SESSION['username']; 
 	$username = $_SESSION['username']; ?>
-	<a href="<?=$linkAddress?>"><?=$username?></a>
-	<a href="../actions/action_logout.php">Logout</a>
+	<a class="header_link" href="../actions/action_logout.php">Logout</a>
+	<a class="header_link" href="<?=$linkAddress?>"><?=$username?></a>
