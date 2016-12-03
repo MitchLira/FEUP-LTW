0665492e1611ec5533ee39bb41d0d6b80bb8e85d
Miriam Gonçalves <margaitizinha_cbt@hotmail.com>
2016-12-03 22:44:11 +0000

    template necessario

--- templates/edit_user_password.php
+++ templates/edit_user_password.php
@@ -1,19 +1,20 @@
-<form action="../actions/save_user_password.php" method="post" id="editProfile">
+<form action="../actions/save_user_password.php" method="post" id="editPassword">
     
     <input type="hidden" name="username" value="<?=$user['username']?>">
 
     <label>Current Password:
-        <input type="password" name="password">
+        <input type="password" name="password" id="password" required  >
     </label>
+    <p class="info"></p>
 
     <label>New Password:
-        <input type="password" name="new_password">
+        <input type="password" name="new_password" id="new_password" required >
     </label>
 
     <label>Confirm Password:
-        <input type="password" name="confirm_password">
+        <input type="password" name="confirm_password" id="confirm_password" required >
     </label>
 
-     <input type="submit" name="submit" value="Save" id="buttonSave" >
+    <input type="submit" name="submit" value="Save" id="buttonSave" disabled>
 
 </form>
