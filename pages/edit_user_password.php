567d405e45d058805debdf979406a6bac982665c
Miriam Gonçalves <margaitizinha_cbt@hotmail.com>
2016-12-03 18:12:23 +0000

    created new edit_profile page

---
+++ pages/edit_user_password.php
@@ -0,0 +1,18 @@
+<?php
+  include_once('../database/connection.php');
+  include_once('../database/user.php');
+  include_once('../database/restaurants.php');
+  include_once('../database/reviews.php');
+
+  $username = $_SESSION['username'];
+  try {
+		$user = getUser($dbh, $username);
+	} catch(PDOException $e) {
+		die($e->getMessage());
+ }
+
+  $cssPath = "../css/edit_profile.css";
+    include ('../templates/header.php');
+    include ('../templates/edit_user_password.php');
+    include ('../templates/footer.php');
+?>
---
+++ templates/edit_user_password.php
@@ -0,0 +1,19 @@
+<form action="../actions/save_profile.php" method="post" id="editProfile">
+    
+    <input type="hidden" name="username" value="<?=$user['username']?>">
+
+    <label>Current Password:
+        <input type="password" name="password">
+    </label>
+
+    <label>New Password:
+        <input type="password" name="new_password">
+    </label>
+
+    <label>Confirm Password:
+        <input type="password" name="confirm_password">
+    </label>
+
+     <input type="submit" name="submit" value="Save" id="buttonSave" >
+
+</form>
