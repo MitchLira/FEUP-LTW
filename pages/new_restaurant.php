6060b92e34e5354fd128803422fec6f5a7c33617
Miriam Gonçalves <margaitizinha_cbt@hotmail.com>
2016-12-07 15:37:40 +0000

    a

--- pages/new_restaurant.php
+++ pages/new_restaurant.php
@@ -2,7 +2,7 @@
     include_once('../database/connection.php');
 	include_once('../database/restaurants.php');
 
-    $cssPath = "../css/home.css";
+    $cssPath = "../css/new_restaurant.css";
     include ('../templates/header.php');
     include ('../templates/new_restaurant.php');
     include ('../templates/footer.php');
